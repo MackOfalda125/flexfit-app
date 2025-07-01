bb2d440c53e311246bdbb2f70795d602b023603c
MackOfalda125 <ofaldamackenzie@gmail.com>
2025-07-01 23:12:26 +0800

    Fix android status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:app/features/loading/loading_screen.dart';
 import 'package:app/features/home/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
   runApp(const MyApp());
 }
 
